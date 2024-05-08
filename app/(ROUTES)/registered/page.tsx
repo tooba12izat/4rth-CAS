23c06262ba62c565d95d7a292d468fe8447b551c
tooba12izat <83787315+tooba12izat@users.noreply.github.com>
2024-05-08 10:17:47 -0700

    did change

--- app/(ROUTES)/registered/page.tsx
+++ app/(ROUTES)/registered/page.tsx
@@ -5,15 +5,15 @@ const Page = async () => {
 
     return (
         <>
-            <div className="registered">REGISTERED SUCCESSFULLY !</div>
-            <div className="buttonn">
-            <button className="playerList-button">
-                <Link href="/players">
-                    <p>Registered Players List</p>
-                </Link>
-            </button>
+            <div className="registered text-2xl md:text-4xl text-center text-blue-400">REGISTERED SUCCESSFULLY !</div>
+            <div className="buttonn flex justify-center mt-10">
+                <button className="playerList-button px-4 py-2 bg-blue-400 text-white rounded-md">
+                    <Link href="/players">
+                        <p>Registered Players List</p>
+                    </Link>
+                </button>
             </div>
-            
+
         </>
     )
 }
